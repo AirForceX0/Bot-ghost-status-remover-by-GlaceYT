2157953d8d4f7fbd3d928ab326206c4c7cd40baa
AirForceX0 <amazingzaid21857@gmail.com>
2024-08-08 11:17:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Playing In A Motorville Session","Listening To Copmlaints"];
 
 
 let currentIndex = 0;
